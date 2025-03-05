d294006855bec51a2e9dc0703739f9d61c523cb3
daichi <mochimochi989@gmail.com>
2025-03-05 16:09:23 +0900

    🎨 データをfetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getAllPokemon,getPokemon } from './utils/pokemon';
 
 function App() {
+const initialURL = "https://pokeapi.co/api/v2/pokemon"
+const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  
+  const fetchPokemonData = async () => {  
+    //ポケモンの全データを取得      
+    const res = await getAllPokemon(initialURL);
+    //各ポケモンの詳細データを取得
+    loadPokemon(res.results);
+    // console.log(res.results);
+    setLoading(false); 
+  }
+  fetchPokemonData();
+
+},[])
+const loadPokemon =async(data)=>{
+  const _pokemonData = await Promise.all(
+    data.map((pokemon)=>{
+      const pokemonRecord = getPokemon(pokemon.url)
+      return pokemonRecord
+}) 
+  )
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {loading ? <h1>ローディング中</h1> : <h1>データ取得完了</h1>}
     </div>
   );
 }
