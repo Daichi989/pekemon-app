c86e66578b5b5d9e3535ecb6d9e917596eb470f8
daichi <mochimochi989@gmail.com>
2025-03-08 23:28:28 +0900

    ポケモンの画像取得

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Card = (pokemon) => {
+const Card = ({pokemon}) => {
   return (
-    <div>
-      Card
+    <div className='card'>
+        <div className='cardImg'>
+            <img src={pokemon.sprites.front_default} alt='' />
+        </div>
+        <h3 className='cardName'>{pokemon.name}</h3>
+        <div className='cardTypes'>
+            
+        </div>
     </div>
   )
 }
